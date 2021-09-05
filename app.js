8adeafa0be4539dd358d1c26e95d717c6d233682
Adi4999 <78524367+Adi4999@users.noreply.github.com>
2021-09-05 19:04:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const body=document.querySelector('body');
+const first =document.querySelector('.first');
+const eventkey=document.querySelector('.event-key');
+const eventkeycode=document.querySelector('.event-key-code');
+const eventcode=document.querySelector('.event-code');
+const eventlocation=document.querySelector('.event-location');
+
+
+body.addEventListener('keydown',function(e){
+    // e.preventDefault();
+
+    console.log(e);
+    console.log(e.key);
+    console.log(e.keyCode);
+    let t=e.code;
+    console.log(t);
+    console.log(e.location);
+   
+  if(e.keyCode==32){
+    first.textContent=e.keyCode;
+    eventkey.style.fontSize="20px"
+    eventkey.textContent="space character";
+    eventkeycode.textContent=e.keyCode;
+    eventcode.textContent=e.code;
+    eventlocation.textContent=e.location;
+  }
+
+  else{
+    eventkey.style.fontSize="150%"
+    first.textContent=e.keyCode;
+    eventkey.textContent=e.key;
+    eventkeycode.textContent=e.keyCode;
+    eventcode.textContent=e.code;
+    eventlocation.textContent=e.location;
+  }
+
+    
+})
+
+
